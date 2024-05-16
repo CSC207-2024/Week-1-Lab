637a41239a1db68fc56fe4af5a17c19e38501062
Joe Fang <25814618+MinecraftFuns@users.noreply.github.com>
2024-05-16 18:35:30 -0400

    [chore] two ways of fixing call to FizzBuzz.main()

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
 //        FizzBuzz.next(5);
         FizzBuzz.main(args);
+        // or
+//        FizzBuzz.main(new String[0]);
     }
 }
