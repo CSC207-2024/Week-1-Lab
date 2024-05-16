13ed6207bc29acab69496ce5dda2494d4191e356
Joe Fang <25814618+MinecraftFuns@users.noreply.github.com>
2024-05-16 18:19:18 -0400

    [refactor] extract method

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -6,16 +6,21 @@ class FizzBuzz {
     public static void main(String[] args) {
         int i = 1;
         while (i < 100) {
-            if (i % 3 == 0 && i % 5 == 0) {
-                System.out.println("FizzBuzz");
-            } else if (i % 3 == 0) {
-                System.out.println("Fizz");
-            } else if (i % 5 == 0) {
-                System.out.println("Buzz");
-            } else {
-                System.out.println(i);
-            }
-            i += 1;
+            i = next(i);
         }
+    }
+
+    private static int next(int i) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            System.out.println("FizzBuzz");
+        } else if (i % 3 == 0) {
+            System.out.println("Fizz");
+        } else if (i % 5 == 0) {
+            System.out.println("Buzz");
+        } else {
+            System.out.println(i);
+        }
+        i += 1;
+        return i;
     }
 }
